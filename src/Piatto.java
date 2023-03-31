c384f2f72e45d0a0790b02bdc71d454edf067a7d
alessiocapriotti <s1093075@studenti.univpm.it>
2023-03-31 11:26:04 +0200

    Task 2

--- src/Piatto.java
+++ src/Piatto.java
@@ -86,6 +86,95 @@ public class Piatto {
 }
 
 
+import java.util.*;
+
+public class MisterCuoco {
+    private List<Partecipante> partecipanti;
+    private List<Chef> giudici;
+    private List<Piatto> piatti;
+
+    public MisterCuoco(List<Partecipante> partecipanti, List<Chef> giudici, List<Piatto> piatti) {
+        this.partecipanti = partecipanti;
+        this.giudici = giudici;
+        this.piatti = piatti;
+    }
+
+    public void calcolaBonus() {
+        for (Piatto piatto : piatti) {
+            int bonus = 0;
+            for (Chef chef : giudici) {
+                if (chef.puòValutare(piatto)) {
+                    bonus += chef.calcolaBonus(piatto);
+                }
+            }
+            piatto.setBonus(bonus);
+        }
+    }
+
+    public boolean verificaCondizioni(int p, int q) {
+        // Condizione 1
+        int piattiConMediaMinoreDiP = 0;
+        for (Piatto piatto : piatti) {
+            int media = (piatto.getAspetto() + piatto.getConsistenza() + piatto.getGusto()) / 3;
+            if (media < p) {
+                piattiConMediaMinoreDiP++;
+            }
+        }
+        if (piattiConMediaMinoreDiP > q) {
+            return false;
+        }
+
+        // Condizione 2
+        for (Partecipante partecipante : partecipanti) {
+            int tecnica = partecipante.getTecnica();
+            int creatività = partecipante.getCreatività();
+            if (tecnica < p && creatività < p) {
+                return false;
+            }
+            if (tecnica < p) {
+                partecipante.incrementaCreatività(q / 2);
+            }
+            if (creatività < p) {
+                partecipante.incrementaTecnica(q / 2);
+            }
+        }
+
+        // Condizione 3
+        Map<Piatto, Integer> conteggioPartecipantiPerPiatto = new HashMap<>();
+        for (Piatto piatto : piatti) {
+            conteggioPartecipantiPerPiatto.put(piatto, 0);
+        }
+        for (Partecipante partecipante : partecipanti) {
+            Set<Piatto> piattiDelPartecipante = partecipante.getPiatti();
+            for (Piatto piatto : piattiDelPartecipante) {
+                int conteggio = conteggioPartecipantiPerPiatto.get(piatto);
+                if (conteggio >= 2) {
+                    return false;
+                }
+                conteggioPartecipantiPerPiatto.put(piatto, conteggio + 1);
+            }
+        }
+
+        // Condizione 4
+        for (Partecipante partecipante : partecipanti) {
+            boolean haBonus = false;
+            Set<Piatto> piattiDelPartecipante = partecipante.getPiatti();
+            for (Piatto piatto : piattiDelPartecipante) {
+                if (piatto.getBonus() > 0) {
+                    haBonus = true;
+                    break;
+                }
+            }
+            if (!haBonus) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class Part
 
 
 
